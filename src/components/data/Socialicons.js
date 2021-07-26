6deceb08daaf01416cf64d258d53e83cbe40128b
varahasai <sachinrohit145@gmail.com>
2021-07-26 22:46:30 +0530

    added gif icons

--- src/components/data/Socialicons.js
+++ src/components/data/Socialicons.js
@@ -1,23 +1,23 @@
 export const SocialData=[
     {
         title:"Instagram",
-        icon:require('../../assests/icons/instagram.png').default,
+        icon:require('../../assests/icons/insta.gif').default,
         link:'https://www.instagram.com/varahasai/',
     },{
         title:"Facebook",
-        icon:require('../../assests/icons/facebook.png').default,
+        icon:require('../../assests/icons/face.gif').default,
         link:'https://www.facebook.com/varahasai45/',
     },{
         title:"LinkedIn",
-        icon:require('../../assests/icons/linkedin.png').default,
+        icon:require('../../assests/icons/in.gif').default,
         link:'https://www.linkedin.com/in/adireddi-varaha-sai-b5ab051a9/',
     },{
         title:"Github",
-        icon:require('../../assests/icons/github.png').default,
+        icon:require('../../assests/icons/git.gif').default,
         link:'https://github.com/Varahasai5S0',
     },{
         title:"Twitter",
-        icon:require('../../assests/icons/twitter.png').default,
+        icon:require('../../assests/icons/twit.gif').default,
         link:'https://twitter.com/VarahaSai1',
     }
 ]
